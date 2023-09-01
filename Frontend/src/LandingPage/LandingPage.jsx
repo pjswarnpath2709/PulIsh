d278a827368e62e03da18d2253e44c76aebdf639
skyi9 <88197965+skyi9@users.noreply.github.com>
2023-09-02 03:26:29 +0530

    service section done

--- Frontend/src/LandingPage/LandingPage.jsx
+++ Frontend/src/LandingPage/LandingPage.jsx
@@ -2,13 +2,17 @@ import "./LandingPage.css";
 import heroImg from "../images/hero1.gif";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import { useEffect, useState } from "react";
+import service1 from "../images/website_builder.svg";
+import service2 from "../images/personal_website.svg";
+import service3 from "../images/cms.svg";
+import service4 from "../images/wordpress.svg";
 
 const LandingPage = () => {
   const [hamburger, setHamburger] = useState(window.innerWidth <= 640);
 
   useEffect(() => {
     const handleResize = () => {
-      setHamburger(window.innerWidth <= 640);
+      setHamburger(window.innerWidth <= 702);
     };
 
     window.addEventListener("resize", handleResize);
@@ -43,8 +47,19 @@ const LandingPage = () => {
         </nav>
         <section className="hero">
           <div className="left">
-            <p>{"We're "}</p>
+            <div style={{ fontWeight: "bolder" }}>{"We're "}</div>
             <h1>Website builders</h1>
+            <p
+              style={{
+                wordWrap: "break-word",
+                color: "#777",
+                marginBottom: "1rem",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore, perferendis neque consequatur temporibus a delectus amet
+              dolorem impedit provident
+            </p>
             <br />
             <a href="">Order now</a>
           </div>
@@ -53,6 +68,71 @@ const LandingPage = () => {
           </div>
         </section>
       </header>
+      <section>
+        <div id="services">
+          <div className="services-upper">
+            <div>
+              <h1>Services Title</h1>
+              <div className="border"></div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+              facere, voluptatibus fugit sit a natus optio? Quis, fugit
+              pariatur. Laboriosam excepturi quos impedit eligendi! Fugit,
+              sequi. Veniam quos officiis, earum ipsa maxime itaque aperiam odio
+              suscipit repellat. Consequuntur corporis accusantium, aliquam
+              deleniti excepturi illo tenetur delectus quam provident
+              reprehenderit iure.
+            </p>
+          </div>
+          <div className="services-lower">
+            <div>
+              <div>
+                <img src={service1} alt="" />
+                <h3>SUBTITLE</h3>
+                <h2>Web-dev</h2>
+                <p className="service-description">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Lorem, ipsum.
+                </p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src={service2} alt="" />
+                <h3>SUBTITLE</h3>
+                <h2>Web-dev</h2>
+                <p className="service-description">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Lorem, ipsum.
+                </p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src={service3} alt="" />
+                <h3>SUBTITLE</h3>
+                <h2>Web-dev</h2>
+                <p className="service-description">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Lorem, ipsum.
+                </p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src={service4} alt="" />
+                <h3>SUBTITLE</h3>
+                <h2>Web-dev</h2>
+                <p className="service-description">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Lorem, ipsum.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
