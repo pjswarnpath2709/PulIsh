d93f21c43e5e6cc57f66982c977aa7ef14480ba3
skyi9 <88197965+skyi9@users.noreply.github.com>
2023-09-03 20:08:26 +0530

    login and signup form done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import LandingPage from "./LandingPage/LandingPage";
+// import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+// import LandingPage from "./LandingPage/LandingPage";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      {/* <LandingPage /> */}
+      {/* <Login /> */}
+      <Register />
     </>
   );
 }
