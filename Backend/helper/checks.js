b3e58485f965f6a0cd85650d7d9f524ba13893e4
pjswarnpath2709 <pjswarnpath@gmail.com>
2023-09-12 00:45:23 +0530

    resolved errors

--- Backend/helper/checks.js
+++ Backend/helper/checks.js
@@ -1,5 +1,3 @@
-import emailValidator from "deep-email-validator";
-
 export const isMissing = (...values) => {
   return values.some((val) => {
     return val === null || val === undefined || val === "" || val === NaN;
